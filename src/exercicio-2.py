e9475defa8e03e3af329b08db3b78d1a64844c14
AlexandreMagalhaes1203 <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>
2026-03-20 10:10:14 -0300

    exercicio 2 finalizado

--- src/exercicio-2.py
+++ src/exercicio-2.py
@@ -1,4 +1,5 @@
-""" C = {2,4,6,8,10,12}
+"""
+C = {2,4,6,8,10,12}
 D = {1,2,3,4,5,6,7,8,9}
 
 Determine (C ∪ D) − C.
@@ -13,3 +14,29 @@ Verifique:
 C ⊆ D ?
 
 """
+
+C = {2,4,6,8,10,12}
+D = {1,2,3,4,5,6,7,8,9}
+
+resultado = ((C | D) - C)
+print("Resultado da uniao de C e D, e diferença de  C é :", resultado)
+
+resultado2 = (D - (C & D))
+print("Resultado da interseçao de C e D, e diferença de D é :", resultado2)
+
+cardresultado=len(resultado)
+print("O resultado da cardinalidade do primeiro conjunto é :", cardresultado)
+
+cardresultado2=len(C)
+cardresultado3=len(D)
+resultadocard=(cardresultado2,cardresultado3)
+print("A cardinalidade de de C com D é :",resultadocard)
+
+
+print(6 in C)
+# If e else pode ser substituido por print (x in y) pertence, e print(x not in y), nao pertece.
+
+print(11 not in D)
+
+print(C <= D)
+
